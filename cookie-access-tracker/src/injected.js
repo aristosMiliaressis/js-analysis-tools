9ec4456ce5e2d69ce49ef90f9296c115b3c75be5
aristos <aristosmiliaresis@gmail.com>
2026-03-01 12:58:29 +0200

    cookie-access-tracker: cookieStore support

--- cookie-access-tracker/src/injected.js
+++ cookie-access-tracker/src/injected.js
@@ -1,5 +1,26 @@
 const extensionAPI = typeof browser !== "undefined" ? browser : chrome;
 
+function getHops() {
+	var hops = "";
+	try {
+		if (window.top != window && window.top == window.top) {
+			var w = window;
+			while (top != w) {
+				var x = 0;
+				for (var i = 0; i < w.parent.frames.length; i++) {
+					if (w == w.parent.frames[i]) x = i;
+				};
+				hops = "frames[" + x + "]" + (hops.length ? '.' : '') + hops;
+				w = w.parent;
+			};
+			hops = "top" + (hops.length ? '.' + hops : '')
+		} else {
+			hops = "top";
+		}
+	} catch (e) { }
+	return hops;
+}
+
 (function () {
 	switch (document.contentType) {
 		case 'application/xml':
@@ -17,24 +38,6 @@ const extensionAPI = typeof browser !== "undefined" ? browser : chrome;
 				try { throw new Error(''); } catch (error) { stack = error.stack || ''; }
 				stack = stack.split('\n').slice(2).map(function (line) { return line.trim(); });
 
-				var hops = "";
-				try {
-					if (window.top != window && window.top == window.top) {
-						var w = window;
-						while (top != w) {
-							var x = 0;
-							for (var i = 0; i < w.parent.frames.length; i++) {
-								if (w == w.parent.frames[i]) x = i;
-							};
-							hops = "frames[" + x + "]" + (hops.length ? '.' : '') + hops;
-							w = w.parent;
-						};
-						hops = "top" + (hops.length ? '.' + hops : '')
-					} else {
-						hops = "top";
-					}
-				} catch (e) { }
-
 				var storeEvent = new CustomEvent('cookieAccessTracker', { 'detail': { 
 					assignment: val, 
 					name:val.split('=')[0],
@@ -45,12 +48,49 @@ const extensionAPI = typeof browser !== "undefined" ? browser : chrome;
 					window: window.top == window ? '' : window.name,
 					fullstack: stack,
 					stack: stack[stack.length-1],
-					hops: hops
+					hops: getHops()
 				}});
 				document.dispatchEvent(storeEvent);
 
 				cookieDesc.set.call(document, val);
 			}
 		});
+	}
+
+	const originalCookieStoreSet = CookieStore.prototype.set;
+	CookieStore.prototype.set = function() {
+		try { throw new Error(''); } catch (error) { stack = error.stack || ''; }
+		stack = stack.split('\n').slice(2).map(function (line) { return line.trim(); });
+
+		var details = {
+			origin: window.origin, 
+			href: location.href,
+			window: window.top == window ? '' : window.name,
+			fullstack: stack,
+			stack: stack[stack.length-1],
+			hops: getHops()
+		};
+
+		if (typeof arguments[0] === "string") {
+			details.name = arguments[0];
+			details.value = arguments[1];
+			details.assignment = `${details.name}=${details.value}`;
+			details.attributes = '';
+		} else {
+			details.name = arguments[0].name;
+			details.value = arguments[0].value;
+			details.attributes = (arguments[0].domain == null ? '' : `domain=${arguments[0].domain}; `)
+							   + (arguments[0].path == null ? '' : `path=${arguments[0].path}; `)
+							   + (arguments[0].expires == null ? '' : `expires=${arguments[0].expires}; `)
+							   + (arguments[0].sameSite == null ? '' : `sameSite=${arguments[0].sameSite}; `)
+							   + (arguments[0].partitioned == null ? '' : `partitioned=${arguments[0].partitioned}; `)
+							   + 'Secure';
+			details.assignment = `${details.name}=${details.value}; ${details.attributes}`;
+		}
+
+		var storeEvent = new CustomEvent('cookieAccessTracker', { 'detail': details });
+		document.dispatchEvent(storeEvent);
+
+		return originalCookieStoreSet.apply(this, arguments);
 	}
 })();
